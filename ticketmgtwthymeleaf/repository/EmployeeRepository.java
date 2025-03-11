f47bbadfbfc6436221b06c2d6631de5b0628e11e
MariaLour03 <133254367+MariaLour03@users.noreply.github.com>
2025-03-11 14:21:16 -0500

    Delete ticketmgtwthymeleaf/repository directory

--- ticketmgtwthymeleaf/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ticketmgtwthymeleaf.repository;
-
-import com.ticketmgtwthymeleaf.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-   Employee findByEmail(String email);
-
-}
